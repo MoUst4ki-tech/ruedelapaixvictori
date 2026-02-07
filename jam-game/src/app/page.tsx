be1dff3c72affe554f63a4c3a4c1e0c62346e3ab
moust4ki <moust4ki@epitech.eu>
2026-02-07 04:17:12 +0100

    add

--- jam-game/src/app/page.tsx
+++ jam-game/src/app/page.tsx
@@ -2,14 +2,19 @@
 
 import dynamic from 'next/dynamic';
 
-const Game = dynamic(() => import('@/components/Game'), { ssr: false });
+const NoeGame = dynamic(() => import('@/components/noe_game'), { ssr: false });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black">
       <h1 className="text-4xl font-bold mb-8 text-white">Next.js + Phaser Game</h1>
-      <div className="border-4 border-white rounded-lg overflow-hidden">
-        <Game />
+      <div className="flex flex-col gap-8"> 
+        <div className="border-4 border-white rounded-lg overflow-hidden">
+        </div>
+        <div className="border-4 border-red-500 rounded-lg overflow-hidden">
+          <NoeGame />
+        </div>
+
       </div>
     </main>
   );
